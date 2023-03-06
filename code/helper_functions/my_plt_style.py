e78dec26156219359624eef45aac17c152ec0ef3
Emilio Lehoucq <50888924+emiliolehoucq@users.noreply.github.com>
2023-03-06 08:13:35 -0600

    Add files via upload

---
+++ code/helper_functions/my_plt_style.py
@@ -0,0 +1,34 @@
+# I've only used this function with limited data and haven't tested it --Emilio Lehoucq 3/6/2023
+
+def my_plt_style(ax, title, xgrid = False, ygrid = False, grid = False):
+    """
+    Function to style plot.
+    
+    Input:
+        ax (Axes). Axes of plot.
+        title (string). Title of plot. 
+        xgrid (Boolean). Optional parameter to add x grid to plot. Default is false.
+        ygrid (Boolean). Optional parameter to add y grid to plot. Default is false.
+        grid (Boolean). Optional parameter to add both x grid and y grid to plot. Default is false.
+    Output:
+        None.
+
+    Dependencies:
+        The function doesn't automatically load any library, but it's designed to work with matplotlib (as plt).    
+    """
+    # Bold title:
+    plt.title(title, fontweight = 'bold')
+    
+    # Remove frame around plot:
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    
+    # Add grid:
+    if xgrid == True:
+        ax.xaxis.grid(linestyle = '--', color = 'lightgrey')
+    elif ygrid == True:
+        ax.yaxis.grid(linestyle = '--', color = 'lightgrey')
+    elif grid == True:
+        ax.grid(linestyle = '--', color = 'lightgrey')
