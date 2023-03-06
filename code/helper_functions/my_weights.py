8c37d1be9fc1a415641a8ee0c9165db7bb8cd5e4
Emilio Lehoucq <50888924+emiliolehoucq@users.noreply.github.com>
2023-03-06 10:34:37 -0500

    Update my_weights.py

--- code/helper_functions/my_weights.py
+++ code/helper_functions/my_weights.py
@@ -34,20 +34,20 @@ def weights_univariate(df, col_name, weight_name, percentages = False, rounded =
     if rounded: return round(result, 2)
     else: return result
 
-def weights_bivariate(df, col_1_name, col_2_name, weight_name, percentages = False, rounded = False, verbose = True):
+def weights_bivariate(df, col_1_name, col_2_name, weight_name, out_format = 'counts', rounded = False, verbose = False):
     """
-    Function to calculate cross tabulated weighted proportions or percentages.
+    Function to calculate cross tabulated weighted counts, proportions, or percentages.
     
     Input:
         df (dataframe). Pandas dataframe.
-        col_1_name (string). Name of the first column.
-        col_2_name (string). Name of the second column.
+        col_1_name (string). Name of the first categorical column.
+        col_2_name (string). Name of the second categorical column.
         weight_name (string). Name of the column with the weights.
-        percentages (Boolean). Output percentages rather than proportions (default).
+        out_format (string). Format of the output: 'counts' (default), 'proportions', or 'percentages'.
         rounded (Boolean). Round output to two decimals (defaults to False).
-        verbose (Boolean). Print information to explore association.
+        verbose (Boolean). Print information to explore association (defaults to false).
     Output:
-        Pandas dataframe with weighted proportions or percentages.
+        Pandas dataframe with weighted counts, proportions, or percentages.
 
     Dependencies:
         The function doesn't automatically load any library, but it's designed to work with Pandas (as pd) and matplotlib (as plt).
@@ -55,31 +55,29 @@ def weights_bivariate(df, col_1_name, col_2_name, weight_name, percentages = Fal
     Code inspired from:
         - https://towardsdatascience.com/how-to-analyze-survey-data-with-python-84eff9cc9568
     """
-    # Create cross tabulations:
+    # Unweighted cross tabulations:
     xtab_counts = pd.crosstab(df[col_1_name], df[col_2_name])
-    xtab_normalized = pd.crosstab(df[col_1_name], df[col_2_name], normalize = 'columns')
-    xtab_weighted = pd.crosstab(df[col_1_name], df[col_2_name], df[weight_name], aggfunc = sum, dropna = True, normalize = 'columns')
+    xtab_norm = pd.crosstab(df[col_1_name], df[col_2_name], normalize = 'columns')
+    # Weighted cross tabulations:
+    xtab_counts_w = pd.crosstab(df[col_1_name], df[col_2_name], df[weight_name], aggfunc = sum, dropna = True)
+    xtab_norm_w = pd.crosstab(df[col_1_name], df[col_2_name], df[weight_name], aggfunc = sum, dropna = True, normalize = 'columns')
 
-    # Calculate proportions or percentages:
-    if percentages:
-        result = xtab_weighted * 100
-        xtab_normalized = xtab_normalized * 100
-    else:
-        result = xtab_weighted
-
-    # Round:
-    if rounded:
-        result = round(result, 2)
-        xtab_normalized = round(xtab_normalized, 2)
+    # Format output:
+    if out_format == 'counts': result = xtab_counts_w
+    elif out_format == 'proportions': result = xtab_norm_w
+    else: xtab_norm, result = xtab_norm * 100, xtab_norm_w * 100
+        
+    # Round output:
+    if rounded: xtab_norm, result = round(xtab_norm, 2), round(result, 2)
 
     # Display information:
     if verbose:
         print('UNWEIGHTED TABLES (COUNTS AND NORMALIZED):\n')
         print(xtab_counts, '\n')
-        print(xtab_normalized, '\n')
+        print(xtab_norm, '\n')
         print('WEIGHTED TABLE (NORMALIZED):\n')
         print(result)
-        print('PLOT WITH WEIGHTED COUNTS:')
+        print('PLOT (USING WEIGHTS):')
         plot = result.plot.barh(figsize = (6, 3))
         plt.show()
     
